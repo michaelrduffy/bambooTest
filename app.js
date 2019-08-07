a7b8fdf432b49098adacd45bb96468081b39cdd6
Michael Duffy <duffymr@aston.ac.uk>
2019-08-07 09:52:35 +0100

    Checking Cookies

--- app.js
+++ app.js
@@ -1,41 +1,61 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var session = require('express-session')
+var SHA512 = require("crypto-js/sha512")
 
-var app = express();
+
+var app = express()
+var helmet = require('helmet')
+app.use(helmet())
+
+
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Change to secure: true when using https
+app.use(session({
+  secret: 'bambooSecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+/* GET home page. */
+app.get('/', function (req, res, next) {
+  if (!req.session.signedIn) {
+    req.session.signedIn = true
+    res.render('index', { title: 'Not Signed In' })
+  } else {
+    res.render('index', { title: 'Express' })
+  }
+})
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
